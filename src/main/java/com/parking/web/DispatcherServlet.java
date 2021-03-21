2321435db5e44a3435563a86f63bb1d707c573a1
有闲 <youxian.mzy@koubei.com>
2021-03-21 16:48:27 +0800

    add servlet

---
+++ src/main/java/com/parking/web/DispatcherServlet.java
@@ -0,0 +1,33 @@
+package com.parking.web;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.OutputStreamWriter;
+
+public class DispatcherServlet extends HttpServlet {
+    @Override
+    public void init() throws ServletException {
+        super.init();
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        if (req.getRequestURI().startsWith("/parking")) {
+            OutputStream out = resp.getOutputStream();
+            OutputStreamWriter writer = new OutputStreamWriter(out);
+            writer.write("welcome to lin's parking.");
+            writer.flush();
+        } else {
+            super.doGet(req, resp);
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        super.doPost(req, resp);
+    }
+}
