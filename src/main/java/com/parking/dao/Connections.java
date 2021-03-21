0ac9e54d2c603d2e073767cc4957e7bb1ce51cc2
有闲 <youxian.mzy@koubei.com>
2021-03-21 16:20:47 +0800

    完成Mysql连接

--- src/main/java/com/parking/dao/Connections.java
+++ src/main/java/com/parking/dao/Connections.java
@@ -1,18 +1,26 @@
 package com.parking.dao;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.util.Collections;
 
 public class Connections {
+    private static final Logger LOGGER = LoggerFactory.getLogger(Collections.class);
 
     static {
         try {
             Class.forName("com.mysql.jdbc.Driver");
         } catch (ClassNotFoundException e) {
-            e.printStackTrace();
+            LOGGER.error("加载数据库驱动失败.", e);
         }
-        DriverManager manager = DriverManager.getConnection("jdbc:mysql://localhost:3306/parking", "info", "");
     }
-    public static Connection getConnection() {
+
+    public static Connection getConnection() throws SQLException {
+        Connection conn = DriverManager.getConnection("jdbc:mysql://localhost:3306/parking", "root", "");
+        return conn;
     }
 }
